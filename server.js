8c755c90cf98b96ec964ed86778b59e82990acd6
Vlad50-50 <150382920+Vlad50-50@users.noreply.github.com>
2024-04-21 17:21:00 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const { Server } = require("socket.io");
 
 const chatHtmlFile = fs.readFileSync(path.join(__dirname,"static","shat.htm"));
 const styleCssFile = fs.readFileSync(path.join(__dirname,"static","style.css"));
 const scriptJSFile = fs.readFileSync(path.join(__dirname,"static","script.js"));
 
-const server = http.createServe((req,res) => {
+const server = http.createServer((req,res) => {
     if (req.method == "GET"){
         switch (req.url) {
             case "/" : return res.end(chatHtmlFile);
@@ -19,3 +20,9 @@ const server = http.createServe((req,res) => {
 })
 
 server.listen(3000);
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("A user connected. ID = " + socket.id);
+})
